73784f5313e658f2fbfe30c8485c410a4c16f608
valexq <90932549+valexq@users.noreply.github.com>
2022-08-24 18:08:29 -0500

    Add files via upload

---
+++ AgendaAmigo/AgendaAmigo/Amigo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AgendaAmigo
+{
+    internal class Amigo
+    {
+        private string name, correo;
+        private long id, tel;
+
+        public Amigo()
+        {
+            name = "";
+            correo = "";
+            id = 0;
+            tel = 0;
+        }
+        public Amigo(string name, string correo, long id, long tel)
+        {
+            this.name = name;
+            this.correo = correo;
+            this.id = id;
+            this.tel = tel;
+        }
+
+        public string Name
+        {
+            get{return name;}
+            set{name = value;}
+        }
+        public string Correo
+        {
+            get{ return correo;}
+            set{ correo = ""; }
+        }
+        public long Id
+        {
+            get{return id;}
+            set{ id = 0;}
+        }
+        public long Tel
+        {
+            get{return tel;}
+            set{id = 0;}
+        }
+    }
+    
+}
---
+++ AgendaAmigo/AgendaAmigo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaAmigo
+{
+    class Program
+    {
+
+        public static void imprimir(string mensaje)
+        {
+            Console.WriteLine();
+        }
+
+        public static string leer(string mensaje)
+        {
+            imprimir(mensaje);
+            return Console.ReadLine();
+        }
+
+
+        public static void imprimir(Amigo objA)
+        {
+            imprimir("Nombre: " + objA.Name);
+            imprimir("Cédula: " + objA.Id);
+            imprimir("Telefono: " + objA.Tel);
+            imprimir("Correo: " + objA.Correo);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAMA AGENDA");
+            Amigo amigo1 = new Amigo();
+            amigo1.Name = "Vanessa";
+            amigo1.Correo = "dadas@correo.itm.co";
+            amigo1.Id = 10221921;
+            amigo1.Tel = 312121032;
+            Console.WriteLine(amigo1);
+            Amigo amigo2 = new Amigo("samuel", "samu@core.co", 36987,21010301 );
+            imprimir(amigo2);
+            imprimir(amigo1);
+            Console.ReadKey();
+        }
+
+    }
+        
+}
+
+    
